a0e2551ff0600d1df14f0e0a7bfac051bfd4b3fd
Sedek <sedek.dev@gmail.com>
2019-01-30 15:33:20 +0300

    Lesson 5.5. Передача параметров

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+# from selenium.webdriver.firefox.webdriver import WebDriver
 # from selenium.webdriver.chrome.webdriver import WebDriver
 
 from fixture.group import GroupHelper
@@ -9,19 +10,27 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver = WebDriver(executable_path=r'd:/~Sedek/WebDrivers/geckodriver.exe',
-                                firefox_binary=r'c:/Program Files/Mozilla Firefox/firefox.exe')
+    def __init__(self, browser, base_url):
 #        self.driver = WebDriver(executable_path=r'd:/~Sedek/WebDrivers/chromedriver.exe',
 #                                chrome_binary=r'"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"')
+        if browser == "firefox":
+            self.driver = webdriver.Firefox(executable_path=r'd:/~Sedek/WebDrivers/geckodriver.exe',
+                                firefox_binary=r'c:/Program Files/Mozilla Firefox/firefox.exe')
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Неопределен браузер %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 #        self.new_user = NewUserHelper(self)
 
     def open_page(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
+        driver.get(self.base_url)
         assert "Адресная книга" in driver.title
 
     def destroy(self):
